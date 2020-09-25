3175203d486d4651877944087ce2dc2547ff94fb
Mark Hammond <mhammond@skippinet.com.au>
2020-09-25 16:17:22 +1000

    This is a WIP that hasn't made much P - and I'm stuck :(
    I decided that rather than copy the protobuf build process, we'd just clone
    the generated files along with the hand-written kotlin needed to init
    viaduct - mainly to see how it worked out. In retrospect I probably should
    have just stuck with the protobuf build process - however, there's a
    reasonable chance I would have ended up in the same situation (as that
    still ends up with a .java file).
    
    tl;dr - I can't convince gradle that the new .java file should be used or
    built. I've tried many things (both to gradle and to the file-system) and
    always end up with the same result. A new .kt file works as expected.
    
    So I guess I'm asking for help with 2 things here:
    * WTF isn't my .java file being used or seen?
    * How do you feel about cloning the generated file? Or should I drag the
      build process across too? Keep in mind we believe we'll end up megazording
      soon(-ish) meaning all this can be removed.

---
+++ android/src/main/java/MsgTypes.kt
@@ -0,0 +1,12 @@
+// XXX - I don't want this to exist - it's just to demonstrate markh's
+// gradle cluelessness.
+//
+// Why is this file seen/built, but MsgTypes.java not?
+
+// If you comment these next lines out, HttpConfig fails to find these symbols.
+// IOW - these are found and used - but they're useless - I want the ones in
+// MsgTypes.java!
+object MsgTypes {
+    object Request {}
+    object Response {}
+}
